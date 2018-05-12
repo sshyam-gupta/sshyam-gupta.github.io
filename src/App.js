4581f780a5a1a5867b0ba754dd5417998cdb6a89
Shyam Gupta <sshyam@outlook.com>
2018-05-12 21:36:19 +0530

    Added under construction page

--- src/App.js
+++ src/App.js
@@ -5,14 +5,47 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div id="content">
+          <h1>{`< building />`}</h1>
+          <p>Working day and night!</p>
+        </div>
+        <div id="outerCraneContainer">
+          <div className="buildings">
+            <div></div>
+            <div className="1"></div>
+            <div className="2"></div>
+            <div className="3"></div>
+            <div className="4"></div>
+          </div>
+          <div className="crane craneThree">
+            <div className="line lineOne"></div>
+            <div className="line lineTwo"></div>
+            <div className="line lineThree"></div>
+            <div className="stand"></div>
+            <div className="weight"></div>
+            <div className="cabin"></div>
+            <div className="arm"></div>
+          </div>
+          <div className="crane craneTwo">
+            <div className="line lineOne"></div>
+            <div className="line lineTwo"></div>
+            <div className="line lineThree"></div>
+            <div className="stand"></div>
+            <div className="weight"></div>
+            <div className="cabin"></div>
+            <div className="arm"></div>
+          </div>
+          <div className="crane craneOne">
+            <div className="line lineOne"></div>
+            <div className="line lineTwo"></div>
+            <div className="line lineThree"></div>
+            <div className="stand"></div>
+            <div className="weight"></div>
+            <div className="cabin"></div>
+            <div className="arm"></div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import withTracker from './components/withTracker';
+import { Route, Router } from 'react-router'
+import createBrowserHistory from 'history/createBrowserHistory'
+const history = createBrowserHistory()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={history}>
+        <Route exact path="/" component={withTracker(App, {})} />
+    </Router>
+    , document.getElementById('root'));
 registerServiceWorker();

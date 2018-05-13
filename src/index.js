544266b0542062624955efeb1ec139bc1029e547
Shyam Gupta <sshyam@outlook.com>
2018-05-13 14:30:36 +0530

    Added route for under construction

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import withTracker from './components/withTracker';
-import { Route, Router } from 'react-router'
-import createBrowserHistory from 'history/createBrowserHistory'
-import { unregister as unregisterServiceWorker } from './registerServiceWorker'
-const history = createBrowserHistory()
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import withTracker from "./components/withTracker";
+import { Route, Router, Redirect } from "react-router";
+import createBrowserHistory from "history/createBrowserHistory";
+import { unregister as unregisterServiceWorker } from "./registerServiceWorker";
+const history = createBrowserHistory();
 
-unregisterServiceWorker();
+// unregisterServiceWorker();
 
 ReactDOM.render(
-    <Router history={history}>
-        <Route exact path="/" component={withTracker(App, {})} />
-    </Router>
-    , document.getElementById('root'));
+  <Router history={history}>
+    <div>
+      <Route
+        exact
+        path="/"
+        component={() => {
+          return <Redirect to="under-construction" />;
+        }}
+      />
+      <Route path="/under-construction" component={withTracker(App, {})} />
+    </div>
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
